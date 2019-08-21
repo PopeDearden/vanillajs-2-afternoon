ec0b059bb2be14f0d8149fa2248e3eb56cb6f43f
taylor dearden <taylordearden@gmail.com>
2019-08-21 15:38:16 -0600

    finished piano

---
+++ vanilla-piano/index.js
@@ -0,0 +1,21 @@
+
+
+// window.addEventListener('keypress', function(keyPressEvent) {
+//     const tone = documentquerySelector(`audio [data-key="${keyPressEvent.keyCode}"]`);
+//     if (!tone) return;
+//     const pianoKey = document.querySelector(`.pianoKey[data-key="${keyPressEvent.keyCode}"]`);
+//     tone.currentTime = 0;
+//     tone.play()
+// })
+
+window.addEventListener("keypress", function(keyPressEvent) {
+    const tone = document.querySelector(`audio[data-key="${keyPressEvent.keyCode}"]`);
+    if (!tone) return;
+    const pianoKey = document.querySelector(`.pianoKey[data-key="${keyPressEvent.keyCode}"]`);
+    tone.currentTime = 0;
+    pianoKey.classList.add("pressed")
+    tone.play();
+    setTimeout(function() {
+        pianoKey.classList.remove("pressed")
+    }, 300);
+    })
